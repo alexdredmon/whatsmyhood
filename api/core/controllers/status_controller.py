f6716d7aa163d6450f0198ccf5ffdeab114d3b35
Alex Redmon <alex.redmon@cylera.com>
2020-01-01 16:12:41 -0500

    Status endpoint

--- api/core/controllers/status_controller.py
+++ api/core/controllers/status_controller.py
@@ -4,6 +4,14 @@ from flask import jsonify
 
 
 @app.route('/')
+def root():
+    return jsonify({
+        "service": "whatsmyhood",
+        "status": "ok",
+    })
+
+
+@app.route('/status')
 def status():
     return jsonify({
         "service": "whatsmyhood",
